e979c39751a56fc998634e352fb2b5a783b85dce
Luna <69774917+carlyjb17@users.noreply.github.com>
2022-10-23 19:02:31 +0200

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -9,59 +9,74 @@
 /*   Updated: 2022/10/12 18:26:58 by ltranca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
+
 #include "libft.h"
 
-static int *ft_pallen(char *a, char ch)
+static int *ft_pallen(char *a, char ch) // Hacer free function, hacer una funcion que cuente palabras caracteres y caracteres
 {
     int   *ret;
     int   len;
     int   len2;
+    int   len3;
 
     len = 1;
-    len2 = -1;
+    len2 = strlen(a);
+    len3 = 0;
+    ret = malloc(len2 * sizeof(int) + 1);
+    ret++;
     while(*a)
     {
-        len2++;
+        len3++;
         if (*a == ch)
         {
-            if (len <= 1)
+            if (len3 <= 1)
             {
+                while (*a == ch)
+                    a++;
+                len3 = 0;
                 continue ;
             }
             len++;
+            a--;
+            len3 = 0;
+            *ret = len3;
+            ret++;
         }
+        a++;
     }
-}*/
+    len++;
+    a--;
+    len3 = 0;
+    *ret = len3;
+    ret++;
+    ret = ret - len;
+    *ret = len;
+    return (ret);
+}
 
-/*
 char ** ft_split (char const *s, char c)
 {
     char **ret;
     char *buff;
     int *n;
     int pointer;
-    char *pch;
 
-    pch = &c;
     if (!s || !c)
         return (NULL);
     n = ft_pallen(s, c);
     pointer = -1;
-    ret = ft_calloc(n[0] + 1, sizeof (char *));
+    ret = calloc(n[0] + 1, sizeof (char *));
     n++;
     if (!ret)
         return (NULL);
     while (*s)
     {
         while (*s == c && *s)
-        {
             s++;
-        }
         pointer++;
         ret[pointer] = ft_substr(s, 0, *n);
-        s = s + *n;
         n++;
+        s = s + *n;
     }
     return(ret);
-}*/
+}
