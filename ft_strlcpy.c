ffc03556ebd337a7c49b6b060738f62221c96c69
Luna <69774917+carlyjb17@users.noreply.github.com>
2022-11-11 05:24:49 -0800

    Add files via upload

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -15,24 +15,17 @@
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 
 {
-	size_t	n;
+    size_t len;
 
-	n = 0;
-	if (!(size <= 0))
-	{
-		while (src[n] != '\0')
-		{
-			if (n == size)
-			{
-				n--;
-				break ;
-			}
-			dest[n] = src[n];
-			n++;
-		}
-		dest[n] = '\0';
-	}
-	return (ft_strlen(src));
+    len = ft_strlen(src);
+    if (len + 1 < size)
+        ft_memcpy(dest, src, len + 1);
+    else if (size != 0)
+    {
+        ft_memcpy(dest, src, size - 1);
+        dest[size - 1] = '\0';
+    }
+    return (len);
 }	
 
 /* int main(void)
